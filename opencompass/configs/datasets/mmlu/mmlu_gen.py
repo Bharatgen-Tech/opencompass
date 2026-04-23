7cea2fcd1008160fd447696848fa3e023766fc1b
Dhrumil Shah <92982318+Dhru1001@users.noreply.github.com>
2026-04-23 12:15:14 +0000

    fix: Import issue for MMLU

--- opencompass/configs/datasets/mmlu/mmlu_gen.py
+++ opencompass/configs/datasets/mmlu/mmlu_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .mmlu_openai_simple_evals_gen_b618ea import mmlu_datasets  # noqa: F401, F403
+    from .mmlu_openai_simple_evals_gen_7efa2c import mmlu_datasets  # noqa: F401, F403
